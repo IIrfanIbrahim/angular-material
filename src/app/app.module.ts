84a5d71d5b0db05303e3c7a88a0f2d80f38ba8a8
Intje Irfan Ibrahim <inceirfanibrahim@gmail.com>
2022-05-25 02:35:10 +0800

    feat: add angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+// import { MatSliderModule } from '@angular/material/slider';
+import { MaterialExampleModule } from 'src/material.module';
+
+// import {Component} from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    // MatSliderModule,
+    MaterialExampleModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class CardFancyExample {}
+
+
+
+// @Component({
+//   selector: 'card-fancy-example',
+//   templateUrl: 'card-fancy-example.html',
+//   styleUrls: ['card-fancy-example.css'],
+// })
